c9e6cfcd24cd0e183ad74db1a7745ab20a837c25
Taylor Rosby <taylorrosby@gmail.com>
2020-03-31 21:15:19 -0700

    Create ILogger.php

---
+++ app/Services/Utility/ILogger.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Services\Utility;
+
+interface ILogger 
+{
+    static function getLogger();
+    public static function debug($message, $data);
+    public static function info($message, $data);
+    public static function warning($message, $data);
+    public static function error($message, $data);
+}
+?>
